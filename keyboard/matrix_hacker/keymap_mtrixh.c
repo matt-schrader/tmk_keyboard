9b5e83d6e38a6bab08943b80296c32ec32e0fc3b
Matthew Schrader <matthew.schrader@jda.com>
2015-12-23 16:52:19 -0600

    Initial hacked together keyboard mapping

---
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -0,0 +1,15 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qwerty */
+    KEYMAP(
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL)
+};
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
+    [1] = ACTION_DEFAULT_LAYER_SET(0)  // set qwerty layout
+};
