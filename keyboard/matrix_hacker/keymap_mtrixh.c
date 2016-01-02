460000f92c15373a551243f47bad5021463783b7
Matthew Schrader <SchraderMJ11@gmail.com>
2016-01-01 21:53:09 -0600

    Get the right handed board working

--- keyboard/matrix_hacker/keymap_mtrixh.c
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -3,13 +3,13 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     FN0, RGUI,APP, RCTL)
+        ESC,  1,    2,    3,    4,    5,   6,         7,   8,    9,   0,    MINS,  EQL,  BSPC, \
+        GRV,  LBRC, Q,    W,    E,    R,   T,         Y,   U,    I,   O,    P,     RBRC, BSLS, \
+        F2,   TAB,  A,    S,    D,    F,   G,         H,   J,    K,   L,    SCLN,  QUOT, ENT, \
+        F3,   LSFT, Z,    X,    C,    V,   B,         N,   M,   COMM, DOT,  SLSH,  RSFT, F4, \
+        LCTL,       LGUI, LALT, FN0,       SPC,       SPC,      FN1,  RGUI, APP,         RCTL)
 };
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
-    [1] = ACTION_DEFAULT_LAYER_SET(0)  // set qwerty layout
+    //[0] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
+    //[0] = ACTION_DEFAULT_LAYER_SET(0)  // set qwerty layout
 };
