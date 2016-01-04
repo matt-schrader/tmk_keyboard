754d158ccf8a644b4150f7f4f469004b71fe0ce6
Matthew Schrader <SchraderMJ11@gmail.com>
2016-01-03 20:16:25 -0600

    Fix positioning of right gui

--- keyboard/matrix_hacker/keymap_mtrixh.c
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -7,7 +7,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         GRV,  LBRC, Q,    W,    E,    R,   T,         Y,   U,    I,   O,    P,     RBRC, BSLS, \
         F2,   TAB,  A,    S,    D,    F,   G,         H,   J,    K,   L,    SCLN,  QUOT, ENT, \
         F3,   LSFT, Z,    X,    C,    V,   B,         N,   M,   COMM, DOT,  SLSH,  RSFT, F4, \
-        LCTL,       LGUI, LALT, FN0,       SPC,       SPC,      FN1,  RGUI, APP,         RCTL)
+        LCTL,       LGUI, LALT, FN0,       SPC,       SPC,      FN1,  RALT, RGUI,         RCTL)
 };
 const uint16_t PROGMEM fn_actions[] = {
     //[0] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
