10f4876fc204db5cc6402aa93f52c31c423e955c
Matthew Schrader <matthew.schrader@jda.com>
2016-01-05 08:36:16 -0600

    Move GUI keys to match mac layout, move bottom function keys to the outside instead of next to space bar , Fix function 2-4 keys to be Function keys instead of F2-4

--- keyboard/matrix_hacker/keymap_mtrixh.c
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -1,13 +1,13 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: qwerty */
+    /* 0: qwerty (mac) */
     KEYMAP(
         ESC,  1,    2,    3,    4,    5,   6,         7,   8,    9,   0,    MINS,  EQL,  BSPC, \
         GRV,  LBRC, Q,    W,    E,    R,   T,         Y,   U,    I,   O,    P,     RBRC, BSLS, \
-        F2,   TAB,  A,    S,    D,    F,   G,         H,   J,    K,   L,    SCLN,  QUOT, ENT, \
-        F3,   LSFT, Z,    X,    C,    V,   B,         N,   M,   COMM, DOT,  SLSH,  RSFT, F4, \
-        LCTL,       LGUI, LALT, FN0,       SPC,       SPC,      FN1,  RALT, RGUI,         RCTL)
+        FN2,  TAB,  A,    S,    D,    F,   G,         H,   J,    K,   L,    SCLN,  QUOT, ENT, \
+        FN3,  LSFT, Z,    X,    C,    V,   B,         N,   M,   COMM, DOT,  SLSH,  RSFT, FN4, \
+        FN0,        LCTL, LALT, LGUI,      SPC,       SPC,      RGUI, RALT, RCTL,        FN1)
 };
 const uint16_t PROGMEM fn_actions[] = {
     //[0] = ACTION_MODS_KEY(MOD_RCTL|MOD_RSFT, KC_ESC), // Task(RControl,RShift+Esc)
