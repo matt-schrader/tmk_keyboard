2f4c28f2c515cdacef0dda976f9976f6d6da6f53
Matthew Schrader <SchraderMJ11@gmail.com>
2016-02-02 20:26:13 -0600

    Update addresses for new chip

--- keyboard/matrix_hacker/matrix.c
+++ keyboard/matrix_hacker/matrix.c
@@ -137,45 +137,48 @@ uint8_t matrix_key_count(void)
 
 // Matrix keyboard Pro Micro column connections
 // col:  0   1   2   3   4   5   6   7   8   9  10  11  12  13
-// pin:                              F7  F6  F4 B3  B2  F5  B6
+// pin:                              C6  D4  D7 E6  B4  F4  B5
 static void  init_cols(void)
 {
   // Input with pull-up(DDR:0, PORT:1)
-  DDRF  &= ~(1<<7 | 1<<6 | 1<<5 | 1<<4);
-  PORTF |=  (1<<7 | 1<<6 | 1<<5 | 1<<4);
+  DDRB  &= ~(1<<4 | 1<<5);
+  PORTB |=  (1<<4 | 1<<5);
 
-  DDRB &= ~(1<<3 | 1<<2 | 1<<6);
-  PORTB |= (1<<3 | 1<<2 | 1<<6);
+  DDRC  &= ~(1<<6);
+  PORTC |=  (1<<6);
+
+  DDRD  &= ~(1<<4 | 1<<7);
+  PORTD |=  (1<<4 | 1<<7);
+
+  DDRE  &= ~(1<<6);
+  PORTE |=  (1<<6);
+
+  DDRF  &= ~(1<<4);
+  PORTF |=  (1<<4);
 }
 
 static matrix_row_t read_cols(void)
 {
-  return (PINF&(1<<7) ? 0 : (1<<7)) |
-         (PINF&(1<<6) ? 0 : (1<<8)) |
-         (PINF&(1<<4) ? 0 : (1<<9)) |
-         (PINB&(1<<3) ? 0 : (1<<10)) |
-         (PINB&(1<<2) ? 0 : (1<<11)) |
-         (PINF&(1<<5) ? 0 : (1<<12)) |
-         (PINB&(1<<6) ? 0 : (1<<13));
+  return (PINC&(1<<6) ? 0 : (1<<7)) |
+         (PIND&(1<<4) ? 0 : (1<<8)) |
+         (PIND&(1<<7) ? 0 : (1<<9)) |
+         (PINE&(1<<6) ? 0 : (1<<10)) |
+         (PINB&(1<<4) ? 0 : (1<<11)) |
+         (PINF&(1<<4) ? 0 : (1<<12)) |
+         (PINB&(1<<5) ? 0 : (1<<13));
 }
 
 // Matrix keyboard Pro Micro row connections
 // row:  0   1   2   3   4
-// pin:  C6  D7  E6  B4  D4
+// pin:  F7  B1  B3  B2  B6
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRC  &= ~0b01000000;
-    PORTC &= ~0b01000000;
+    DDRB  &= ~0b01001110;
+    PORTB &= ~0b01001110;
 
-    DDRD  &= ~0b10010000;
-    PORTD &= ~0b10010000;
-
-    DDRE  &= ~0b01000000;
-    PORTE &= ~0b01000000;
-
-    DDRB  &= ~0b00010000;
-    PORTB &= ~0b00010000;
+    DDRF  &= ~0b10000000;
+    PORTF &= ~0b10000000;
 }
 
 static void select_row(uint8_t row)
@@ -183,24 +186,24 @@ static void select_row(uint8_t row)
     // Output low(DDR:1, PORT:0) to select
     switch (row) {
         case 0:
-            DDRC  |= (1<<6);
-            PORTC &= ~(1<<6);
+            DDRF  |= (1<<7);
+            PORTF &= ~(1<<7);
             break;
         case 1:
-            DDRD  |= (1<<7);
-            PORTD &= ~(1<<7);
+            DDRB  |= (1<<1);
+            PORTB &= ~(1<<1);
             break;
         case 2:
-            DDRE  |= (1<<6);
-            PORTE &= ~(1<<6);
+            DDRB  |= (1<<3);
+            PORTB &= ~(1<<3);
             break;
         case 3:
-            DDRB  |= (1<<4);
-            PORTB &= ~(1<<4);
+            DDRB  |= (1<<2);
+            PORTB &= ~(1<<2);
             break;
         case 4:
-            DDRD  |= (1<<4);
-            PORTD &= ~(1<<4);
+            DDRB  |= (1<<6);
+            PORTB &= ~(1<<6);
             break;
     }
 }
