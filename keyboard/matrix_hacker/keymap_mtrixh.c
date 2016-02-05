af7a7c15f2eef7122d01d644969fb17faf208c60
Matthew Schrader <matthew.schrader@jda.com>
2016-02-05 07:45:45 -0600

    fix the index pointer to the function layer

--- keyboard/matrix_hacker/keymap_mtrixh.c
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -17,5 +17,5 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_MOMENTARY(6)
+  [0] = ACTION_LAYER_MOMENTARY(1)
 };
