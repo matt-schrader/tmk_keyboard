415554b90d4d5d3e665d2a2f2ec189244b95a798
Matthew Schrader <SchraderMJ11@gmail.com>
2016-02-06 13:38:39 -0600

    Add delete key, and play/pause key

--- keyboard/matrix_hacker/keymap_mtrixh.c
+++ keyboard/matrix_hacker/keymap_mtrixh.c
@@ -7,9 +7,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         GRV,  LBRC, Q,    W,    E,    R,    T,         Y,    U,    I,    O,    P,     RBRC, BSLS, \
         FN2,  TAB,  A,    S,    D,    F,    G,         H,    J,    K,    L,    SCLN,  QUOT, ENT, \
         FN3,  LSFT, Z,    X,    C,    V,    B,         N,    M,   COMM,  DOT,  SLSH,  RSFT, MUTE, \
-        FN0,        LCTL, LALT, LGUI,       SPC,       SPC,       RGUI,  RALT, RCTL,        FN1),
+        FN0,        LCTL, LALT, LGUI,       SPC,       SPC,       RGUI,  RALT, RCTL,        MEDIA_PLAY_PAUSE),
     KEYMAP(
-        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      TRNS, TRNS, TRNS, TRNS, TRNS,  TRNS, TRNS, \
+        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      TRNS, TRNS, TRNS, TRNS, TRNS,  TRNS, DEL, \
         TRNS, TRNS, TRNS, TRNS, UP,   TRNS, TRNS,      TRNS, TRNS, UP,   TRNS, TRNS,  TRNS, TRNS, \
         TRNS, TRNS, TRNS, LEFT, DOWN, RGHT, TRNS,      TRNS, LEFT, DOWN, RGHT, TRNS,  TRNS, TRNS, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      TRNS, TRNS, TRNS, TRNS, TRNS,  TRNS, TRNS, \
